db80fc196da29a71eb3814e0dd0e645b0536e814
popvilla <192429116+popvilla@users.noreply.github.com>
2026-01-06 22:10:21 -0500

    Refactor installation commands and update README for clarity

--- tests/setup.ts
+++ tests/setup.ts
@@ -52,24 +52,15 @@ Object.defineProperty(URL, 'revokeObjectURL', {
 
 // Mock localStorage
 
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const localStorageMock = (() => {
   let store: { [key: string]: string } = {};
   return {
     /**
      *
      */
-    getItem: (key: string): string | null => store[key] || null,
-    /**
-     *
-     */
     setItem: (key: string, value: string): void => {
       store[key] = value.toString();
-    },
-    /**
-     *
-     */
-    removeItem: (key: string): void => {
-      delete store[key];
     },
     /**
      *
